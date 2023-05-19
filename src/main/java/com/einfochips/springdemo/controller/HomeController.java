5a42c14b1f1c17cc52fd119d3626681e7d2720b3
Jimmykumar Patel <Jimmykumar.Patel@einfochips.com>
2023-05-19 11:39:09 +0530

    Added Tomcat

--- src/main/java/com/einfochips/springdemo/controller/HomeController.java
+++ src/main/java/com/einfochips/springdemo/controller/HomeController.java
@@ -3,12 +3,17 @@ package com.einfochips.springdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HomeController {
 
-	@GetMapping("")
-	public String getHomePage() {
-		return "Welcome to Demo Spring App";
+	@GetMapping("/")
+	public ModelAndView getHomePage() {
+		return new ModelAndView("home");
+	}
+	@GetMapping("/add")
+	public ModelAndView getAddPage() {
+		return new ModelAndView("add");
 	}
 }
---
+++ src/main/webapp/views/add.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Add Page</title>
+</head>
+<body>
+Welcome to Add
+</body>
+</html>
